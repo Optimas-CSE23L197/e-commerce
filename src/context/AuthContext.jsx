1f220e530d82a20f2dc0994f0bd4205295ecfc82
Animesh <161862191+Optimas-CSE23L197@users.noreply.github.com>
2026-01-18 23:40:21 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { Toast } from "../utils/Toast";
+import { Toast } from "../utils/Toast.js";
 
 const AuthContext = createContext();
 
